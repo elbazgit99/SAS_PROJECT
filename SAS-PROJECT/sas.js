2c61a30f58637b87ba8d9056a41baeb574b16039
elbazgit99 <helbaz124@gmail.com>
2026-09-09 10:37:58 +0100

    add functions

--- SAS-PROJECT/sas.js
+++ SAS-PROJECT/sas.js
@@ -1,43 +1,245 @@
 var prompt = require('prompt-sync')();
 
-function addStudent() {
-    console.log("Success");
+const trips = [
+    {
+        id: 1,
+        departure: "Safi",
+        destination: "Youssoufia",
+        departureTime: "07:30",
+        arrivalTime: "08:30",
+        price: 25,
+        availableSeats: 50
+    },
+    {
+        id: 2,
+        departure: "Safi",
+        destination: "Marrakech",
+        departureTime: "08:00",
+        arrivalTime: "10:30",
+        price: 90,
+        availableSeats: 50
+    },
+    {
+        id: 3,
+        departure: "Safi",
+        destination: "Casablanca",
+        departureTime: "09:00",
+        arrivalTime: "13:00",
+        price: 140,
+        availableSeats: 50
+    },
+    {
+        id: 4,
+        departure: "Youssoufia",
+        destination: "Marrakech",
+        departureTime: "09:15",
+        arrivalTime: "11:00",
+        price: 65,
+        availableSeats: 50
+    },
+    {
+        id: 5,
+        departure: "Youssoufia",
+        destination: "Casablanca",
+        departureTime: "10:00",
+        arrivalTime: "13:30",
+        price: 110,
+        availableSeats: 50
+    },
+    {
+        id: 6,
+        departure: "Marrakech",
+        destination: "Casablanca",
+        departureTime: "11:30",
+        arrivalTime: "14:30",
+        price: 120,
+        availableSeats: 50
+    },
+    {
+        id: 7,
+        departure: "Marrakech",
+        destination: "Rabat",
+        departureTime: "12:00",
+        arrivalTime: "16:00",
+        price: 150,
+        availableSeats: 50
+    },
+    {
+        id: 8,
+        departure: "Casablanca",
+        destination: "Rabat",
+        departureTime: "14:00",
+        arrivalTime: "15:15",
+        price: 40,
+        availableSeats: 50
+    },
+    {
+        id: 9,
+        departure: "Casablanca",
+        destination: "Kenitra",
+        departureTime: "15:00",
+        arrivalTime: "16:45",
+        price: 55,
+        availableSeats: 50
+    },
+    {
+        id: 10,
+        departure: "Rabat",
+        destination: "Kenitra",
+        departureTime: "16:00",
+        arrivalTime: "16:45",
+        price: 30,
+        availableSeats: 50
+    },
+    {
+        id: 11,
+        departure: "Rabat",
+        destination: "Fes",
+        departureTime: "17:00",
+        arrivalTime: "19:30",
+        price: 95,
+        availableSeats: 50
+    },
+    {
+        id: 12,
+        departure: "Kenitra",
+        destination: "Fes",
+        departureTime: "17:30",
+        arrivalTime: "20:00",
+        price: 85,
+        availableSeats: 50
+    },
+    {
+        id: 13,
+        departure: "Fes",
+        destination: "Meknes",
+        departureTime: "08:30",
+        arrivalTime: "09:20",
+        price: 35,
+        availableSeats: 50
+    },
+    {
+        id: 14,
+        departure: "Fes",
+        destination: "Oujda",
+        departureTime: "10:00",
+        arrivalTime: "13:30",
+        price: 130,
+        availableSeats: 50
+    },
+    {
+        id: 15,
+        departure: "Meknes",
+        destination: "Rabat",
+        departureTime: "11:00",
+        arrivalTime: "13:30",
+        price: 80,
+        availableSeats: 50
+    },
+    {
+        id: 16,
+        departure: "Meknes",
+        destination: "Casablanca",
+        departureTime: "12:00",
+        arrivalTime: "15:00",
+        price: 105,
+        availableSeats: 50
+    },
+    {
+        id: 17,
+        departure: "Casablanca",
+        destination: "El Jadida",
+        departureTime: "16:30",
+        arrivalTime: "18:00",
+        price: 50,
+        availableSeats: 50
+    },
+    {
+        id: 18,
+        departure: "El Jadida",
+        destination: "Safi",
+        departureTime: "18:30",
+        arrivalTime: "20:30",
+        price: 60,
+        availableSeats: 50
+    },
+    {
+        id: 19,
+        departure: "Marrakech",
+        destination: "Agadir",
+        departureTime: "15:00",
+        arrivalTime: "18:30",
+        price: 100,
+        availableSeats: 50
+    },
+    {
+        id: 20,
+        departure: "Agadir",
+        destination: "Safi",
+        departureTime: "19:00",
+        arrivalTime: "22:00",
+        price: 95,
+        availableSeats: 50
+    }
+];
+const tickets = []
+let counter = 0
+function createTicket() {
+    console.log("createTicket");
+    
+}
+function readTicket() {
+    console.log("readTicket");
+    
+}
+function updateTicket() {
+    console.log("updateTicket");
+    
+}
+function deleteTicket() {
+    console.log("deleteTicket");
+    
+}
+function searchTicket() {
+    console.log("searchTicket");
     
 }
 
-function main() {
-    let n;
+
+function main(n) {
+    
     do {
         console.log("************************");
-        console.log("1. add Student");
-        console.log("2. Afficher Std");
-        console.log("3. Rechercher Std");
-        console.log("4. Modifier Std");
-        console.log("5. Supprimer Std");
-        console.log("6. Quitté");
+        console.log("1. create Student");
+        console.log("2. Read ticket");
+        console.log("3. update ticket");
+        console.log("4. delete ticket");
+        console.log("5. search ticket");
+        console.log("6. Quits");
         console.log("************************");
     
-        n = Number(prompt("Tapez un choix (1-6)"))
+        n = Number(prompt("Type your choice (1-6)"))
         switch (n) {
             case 1:
-                ajoouterStd()
+                createTicket()
                 break;
             case 2:
-                
+                readTicket()
                 break;
             case 3:
-                
+                updateTicket()
                 break;
             case 4:
-                
+                deleteTicket()
                 break;
             case 5:
-                
+                searchTicket()
                 break;
             case 6: break;
         
             default:
-                console.log("Votre reposne n'etait pas acceptable, Svp donne moi une valeur entre 1 et 6");
+                 console.log();
+                  n = Number(prompt("Please Inter a Valide Choice"))
                 break;
         }
     } while (n!=6)
